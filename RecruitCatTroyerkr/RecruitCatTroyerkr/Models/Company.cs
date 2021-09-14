94ae6719c6383f25de096a01ba9149ca063c59ce
Troyerkr <krobtroyer@gmail.com>
2021-09-14 15:34:08 -0400

    Created all the classes then added Id

---
+++ RecruitCatTroyerkr/RecruitCatTroyerkr/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitCatTroyerkr.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+    }
+}
